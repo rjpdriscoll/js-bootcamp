1b772405e0940e85e90a39b388ab04e4a0a6da82
rjpdriscoll <rjpdriscoll@gmail.com>
2019-12-06 21:22:44 -0700

    lecture 106

--- hangman/app.js
+++ hangman/app.js
@@ -1,3 +1,7 @@
+// HTTP (Hypertext Transfer Protocol)
+// Request - what do we want to do
+// Response - what was actually done
+
 // Primitive value: string, number, boolean, null, undefined
 
 // Object prototype chain: myObject --> Object.prototype --> null
@@ -14,9 +18,43 @@ const game1 = new Hangman('car part store', 4);
 puzzleEl.textContent = game1.puzzle;
 guessesEl.textContent = game1.statusMessage;
 
-window.addEventListener('keypress', function (e) {
+window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode);
     game1.getGuess(guess);
     puzzleEl.textContent = game1.puzzle;
     guessesEl.textContent = game1.statusMessage;
 })
+
+// Making an HTTP request
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText);
+        console.log(data);
+    } else if (e.target.readyState === 4) {
+        console.log('An error has taken place');
+    }
+})
+
+request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3');
+request.send();
+
+// Challenge
+const countryCode = 'US';
+
+const countryRequest = new XMLHttpRequest();
+
+countryRequest.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText);
+        const countryNameArray = data.filter((country) => country.alpha2Code === countryCode);
+        const countryName = countryNameArray.pop().name;
+        console.log(countryName);
+    } else if (e.target.readyState === 4) {
+        console.log('An error has occurred.');
+    }
+})
+
+countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
+countryRequest.send();
