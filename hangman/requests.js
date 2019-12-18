b85b40059f6cd45eaf9220adae5b4cc4d123414e
rjpdriscoll <rjpdriscoll@gmail.com>
2019-12-17 22:06:40 -0700

    async/await lecture

--- hangman/requests.js
+++ hangman/requests.js
@@ -1,13 +1,12 @@
-const getPuzzle = (wordCount) => {
-    return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`, {}).then((response) => {
-        if (response.status === 200) {
-            return response.json();
-        } else {
-            throw new Error('Unable to fetch puzzle');
-        }
-    }).then((data) => {
+const getPuzzle = async (wordCount) => {
+    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
+
+    if (response.status === 200) {
+        const data = await response.json();
         return data.puzzle;
-    })
+    } else {
+        throw new Error('Unable to get puzzle');
+    }
 }
 
 const getCountry = (countryCode) => {
