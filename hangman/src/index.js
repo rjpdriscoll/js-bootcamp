0c8e3b8b53e19d49493cc3397c412512deaf9a7f
rjpdriscoll <rjpdriscoll@gmail.com>
2020-01-02 18:33:57 -0700

    middle of third party libs

--- hangman/src/index.js
+++ hangman/src/index.js
@@ -1,5 +1,8 @@
+import uuidv4 from 'uuid/v4';
 import Hangman from './hangman';
 import getPuzzle from './requests';
+
+console.log(uuidv4());
 
 const puzzleEl = document.querySelector('#puzzle');
 const guessesEl = document.querySelector('#guesses');
